7cf5ba1511833b1fcd81a00a5240d48d7ee81876
Dipokal HHJ <hhj@devent.kr>
2023-06-21 21:01:42 +0900

    feat: move player

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from panda3d.core import PlaneNode, Plane, Fog, DirectionalLight
 from math import pi, sin, cos
 import sys
 
+
 class App(ShowBase):
 
     def __init__(self):
@@ -23,9 +24,14 @@ class App(ShowBase):
         plane1_np.show()
         self.scene.setClipPlane(plane1_np)
 
-        myModel = self.loader.loadModel("./models/objects/box.gltf")
-        myModel.reparentTo(self.render)
-        myModel.setPos(6,70,8)
+        self.isMove = False
+        self.modelPosition = [6,70,8]
+
+        self.model = self.loader.loadModel("./models/objects/box.gltf")
+        self.model.reparentTo(self.render)
+        self.model.setScale(10)
+
+        self.model.setPos(self.modelPosition[0], self.modelPosition[1], self.modelPosition[2])
 
 
         myFog = Fog("Fog Name")
@@ -33,17 +39,64 @@ class App(ShowBase):
         myFog.setExpDensity(0.4574)
         self.render.setFog(myFog)
 
-        # terrain = GeoMipTerrain("mySimpleTerrain")
-        # terrain.setHeightfield("heightField.png")
-        # #terrain.setBruteforce(True)
-        # terrain.getRoot().reparentTo(self.render)
-        # terrain.generate()
 
         dlight = DirectionalLight('my dlight')
         dlnp = self.render.attachNewNode(dlight)
 
         self.accept("escape", sys.exit)
+        self.accept("a", self.moveLeft)
+        self.accept("a-up", self.freeze)
+        self.accept("d", self.moveRight)
+        self.accept("d-up", self.freeze)
+        self.accept("w", self.moveForward)
+        self.accept("w-up", self.freeze)
+        self.accept("s", self.moveBack)
+        self.accept("s-up", self.freeze)
 
+        taskMgr.doMethodLater(1/60, self.loop, 'tickTask')
+
+
+    def loop(self, task):
+        self.move()
+        return Task.again
+
+    def move(self):
+        speed = 4
+        if (self.isMove == False):
+            return 0
+        
+        if (self.moveType == 'left'):
+            self.modelPosition[0] -= speed
+        elif (self.moveType == 'right'):
+            self.modelPosition[0] += speed
+        elif (self.moveType == 'forward'):
+            self.modelPosition[1] += speed
+        elif (self.moveType == 'back'):
+            self.modelPosition[1] -= speed
+
+        self.model.setPos(self.modelPosition[0], self.modelPosition[1], self.modelPosition[2])
+
+    def freeze(self):
+        self.isMove = False
+        self.moveType = 'none'
+
+
+    def moveLeft(self):
+        self.isMove = True
+        self.moveType = 'left'
+
+
+    def moveRight(self):
+        self.isMove = True
+        self.moveType = 'right'
+
+    def moveForward(self):
+        self.isMove = True
+        self.moveType = 'forward'
+
+    def moveBack(self):
+        self.isMove = True
+        self.moveType = 'back'
 
 
 app = App()
